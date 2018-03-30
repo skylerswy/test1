462e6c3386936dc1fa97eb668043d2973421762e
skylerswy <56240597@qq.com>
2018-03-30 13:18:24 +0800

    规范代码

--- js/new_file.js
+++ js/new_file.js
@@ -1,4 +1,4 @@
-var num=0;
+var num = 0;
 
 
 // var bt=document.getElementsByClassName("button");
@@ -15,7 +15,7 @@ var num=0;
 // }
 
 
-//pt.onkeydown=function(e){
+// pt.onkeydown=function(e){
 // 	var keyCode = e.keyCode;
 // 	document.getElementById("screenName").value="";   
 // 	return false;
@@ -23,58 +23,60 @@ var num=0;
 
 
 //var pt=document.getElementById("screenName");
-var pt=$("screenName");
+var pt = $("screenname");
 
-function putin(num){
-	document.getElementById("screenName").value+=num;
-	var numdel=document.getElementById("screenName").value;
+function putin(num) {
+	document.getElementById("screenname").value += num;
+	var numdel = document.getElementById("screenname").value;
 	console.log(numdel);
-	window.data=numdel;
+	window.data = numdel;
 	return data;	
 }
 
-data=0;
+data = 0;
 
-function delit(){
-	var numdeling=window.data;
+function delit() {
+	var numdeling = window.data;
 	console.log(numdeling);
-	var newnum=Math.floor(numdeling/10);
-	if(newnum==0){
-		document.getElementById("screenName").value="";
-		window.data=0;
-	}else{
-	console.log(newnum);
-	document.getElementById("screenName").value="";
-	document.getElementById("screenName").value=newnum;
-	window.data=newnum;
+	var newnum = Math.floor(numdeling/10);
+	if (newnum == 0){
+		document.getElementById("screenname").value = "";
+		window.data = 0;
+	}
+	else {
+	    console.log(newnum);
+	    document.getElementById("screenname").value = "";
+	    document.getElementById("screenname").value = newnum;
+	    window.data = newnum;
 	}
 	return data;
 }
 
-tmpe=0;
+tmpe = 0;
 
 
 
-function confirm(){
+function confirm() {
 	/*var tmpe=data;*/
-	var res=data-tmpe;
-	tmpe=data;
+	var res = data - tmpe;
+	tmpe = data;
 	
 	console.log(res);
-	if(res!=1){
+	if (res != 1){
  		alert("不连续，请重新开始");
- 		document.getElementById("screenName").value="";
- 		tmpe=0;
+ 		document.getElementById("screenname").value = "";
+ 		tmpe = 0;
  		// document.getElementById("screennote").value=tmpe;
- 	}else{
+	 }
+	 else {
  		console.log("连续可继续");
- 		document.getElementById("screenName").value="";
+ 		document.getElementById("screenname").value = "";
  		// document.getElementById("screennote").value=tmpe;
  	}
  	
  }
 
-var c=0;
+var c = 0;
 var t;
 // function timedCount()
 // {
@@ -89,24 +91,25 @@ var t;
 // 	}
 // }
 // setinterval替代 settimeout   
-//setTimeout(表达式,延时时间)在执行时,是在载入后延迟指定时间后,去执行一次表达式
-//而setInterval(表达式,交互时间)则不一样,它从载入后,每隔指定的时间就执行一次表达式
+// setTimeout(表达式,延时时间)在执行时,是在载入后延迟指定时间后,去执行一次表达式
+// 而setInterval(表达式,交互时间)则不一样,它从载入后,每隔指定的时间就执行一次表达式
 
-function Count(){
-	if(c<10){
-	c=c+1;	
-	document.getElementById('time-text').value=c;
+function Count() {
+	if (c<10){
+	c = c+1;	
+	document.getElementById('time-text').value = c;
 	console.log(c);
-}else{
+}
+else {
 	c=0;
-	document.getElementById('time-text').value=c;
+	document.getElementById('time-text').value = c;
 	clearInterval(t);
 	alert("时间到")
 }
 }
 
-function timedCount(){
-	t=setInterval("Count()",1000);
+function timedCount() {
+	t = setInterval("Count()",1000);
 	// function Count(){
 	// 	if(c<=10){
 	// 	document.getElementById('time-text').value=c;
